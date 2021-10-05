3642fa1193ecf9d0d594fd53b80dfb9ceaedc6c0
JTTran <tranjt80@gmail.com>
2021-10-06 00:28:27 +0300

    added REST Controller Methods to Find and Add Employee

--- src/main/java/com/tranjt/springboot/crudedemo/rest/EmployeeRestController.java
+++ src/main/java/com/tranjt/springboot/crudedemo/rest/EmployeeRestController.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +29,30 @@ public class EmployeeRestController {
 	@GetMapping("/employees")
 	public List<Employee> findAll() {
 		return employeeService.findAll();
+	}
+	
+	// add mapping for GET /employees/{employeeId}	
+	@GetMapping("/employees/{employeeId}")
+	public Employee getEmployee(@PathVariable int employeeId) {
+		
+		Employee theEmployee = employeeService.findById(employeeId);
+		
+		if (theEmployee == null) {
+			throw new RuntimeException("Employee id not found - " + employeeId);
+		}
+		return theEmployee;
+	}
+	
+	// add mapping for POST /employees - add new employee
+	@PostMapping("/employees")
+	public Employee addEmployee(@RequestBody Employee theEmployee) {
+		
+		//also just  incase they pass an id in JSON ... set id to 0
+		// this is to focus save of new item  ... instead of update
+		theEmployee.setId(0);
+		
+		employeeService.save(theEmployee);
+		return  theEmployee;
 	}
 
 }
