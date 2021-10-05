aa843a662e7ac519988d7ed8bb330959ce78b29f
JTTran <tranjt80@gmail.com>
2021-10-05 22:26:47 +0300

    init files

---
+++ src/main/java/com/tranjt/springboot/crudedemo/CrudedemoApplication.java
@@ -0,0 +1,13 @@
+package com.tranjt.springboot.crudedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudedemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tranjt/springboot/crudedemo/CrudedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tranjt.springboot.crudedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
